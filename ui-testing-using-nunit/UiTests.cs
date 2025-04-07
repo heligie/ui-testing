0b8ccf9a08dd75569a794f65e1280933255a0e46
heligie <heligiette@gmail.com>
2025-04-07 22:17:21 +0300

    feat: initialize WebDriver in the setup method and add the teardown method

--- ui-testing-using-nunit/UiTests.cs
+++ ui-testing-using-nunit/UiTests.cs
@@ -1,15 +1,29 @@
-﻿namespace ui_testing_using_nunit;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ui_testing_using_nunit;
 
 public class Tests
 {
+    private IWebDriver _driver;
+
     [SetUp]
     public void Setup()
     {
+        _driver = new ChromeDriver();
+        _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
     }
 
     [Test]
     public void Test1()
     {
         Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _driver?.Quit();
+        _driver?.Dispose();
     }
 }
