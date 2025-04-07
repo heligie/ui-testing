e441f348024688c952199c7dd206566dce3c8aa8
heligie <heligiette@gmail.com>
2025-04-07 22:22:24 +0300

    feat: add login method

--- ui-testing-using-nunit/UiTests.cs
+++ ui-testing-using-nunit/UiTests.cs
@@ -1,17 +1,34 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace ui_testing_using_nunit;
 
 public class Tests
 {
     private IWebDriver _driver;
+    private WebDriverWait _wait;
 
     [SetUp]
     public void Setup()
     {
         _driver = new ChromeDriver();
         _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+
+        Login("", "");
+    }
+
+    private void Login(string username, string password) 
+    {
+        _driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
+        _driver.FindElement(By.Id("Username")).SendKeys(username);
+        _driver.FindElement(By.Id("Password")).SendKeys(password);
+        _driver.FindElement(By.Name("button")).Click();
+
+        _wait.Until(ExpectedConditions.UrlToBe("https://staff-testing.testkontur.ru/news"));
     }
 
     [Test]
