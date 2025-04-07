ae61ba3d0d587a0d7a6981977bdb88dbdd7df657
heligie <heligiette@gmail.com>
2025-04-07 22:12:38 +0300

    feat: create a new project for testing using NUnit

---
+++ ui-testing-using-nunit/UiTests.cs
@@ -0,0 +1,15 @@
+﻿namespace ui_testing_using_nunit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
